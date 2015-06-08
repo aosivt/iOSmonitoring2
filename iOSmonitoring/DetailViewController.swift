79bf46a990582841f2ab8d5a8ee8cb8f746dc364
Ощепков Александр Юрьевич <au.oschepkov@roberts-Mac.local>
2015-06-08 13:48:19 +0300

    Initial Commit

---
+++ iOSmonitoring/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  iOSmonitoring
+//
+//  Created by Ощепков Александр Юрьевич on 08.06.15.
+//  Copyright (c) 2015 Ощепков Александр Юрьевич. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    var detailItem: AnyObject? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail: AnyObject = self.detailItem {
+            if let label = self.detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
